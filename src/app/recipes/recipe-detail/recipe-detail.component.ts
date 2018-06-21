1e375e0ab2717cfb0c867598e901e803fa4c98c4
Mike Phelps <jmphelpsjr@gmail.com>
2018-06-21 16:30:28 -0400

    added routing for recipes and shopping-list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,20 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipesService } from '../recipes.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent implements OnInit {
- @Input() recipe: Recipe;
+recipe: Recipe;
+id: number;
+
+ constructor(private recipeService: RecipesService,
+             private route: ActivatedRoute) { }
 
  ngOnInit() {
-
+  this.route.params.subscribe(
+    (params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    }
+  );
  }
- constructor(private recipeService: RecipesService) { }
 
  onAddToShoppingList() {
   this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
  }
+
 }
